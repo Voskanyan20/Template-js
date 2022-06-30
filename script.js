9425a2e4a32e739fff74e02a8201fd470aa3e48e
Voskanyan20 <99832154+Voskanyan20@users.noreply.github.com>
2022-07-01 02:45:56 +0400

    template with js

---
+++ script.js
@@ -0,0 +1,194 @@
+var main = document.createElement("div")
+main.style.backgroundImage = "url(./image/01.jpg)"
+main.style.backgroundSize = "cover"
+main.style.backgroundPosition = "center"
+main.style.backgroundRepeat = "no-repeat"
+    var header = document.createElement("div")
+        header.style.width = "90%"
+        header.style.height = "60px"
+        header.style.display = "flex"
+        header.style.margin = "0 auto"
+        header.style.justifyContent = "space-between"
+        header.style.alignItems = "center"
+        var logo = document.getElementById("logo")
+        header.appendChild(logo)
+        var headerList = document.getElementById("header__list")
+        header.appendChild(headerList)
+        function f(x){
+            x.style.color = "#17bed2"
+            x.style.transition = "all 0.4s ease"
+        }
+        function j(y){
+            y.style.color = "white"
+        }
+    main.appendChild(header)
+    var mainH1 = document.createElement("h1")
+        mainH1.innerHTML = "HI-TECH DESIGN"
+        mainH1.style.fontSize = "80px"
+        mainH1.style.fontWeight = "700"
+        mainH1.style.color = "white"
+        mainH1.style.lineHeight = "1.1"
+        mainH1.style.fontFamily = "monospace"
+        mainH1.style.margin = "80px 0px 20px 80px"
+    main.appendChild(mainH1)
+    var mainP = document.createElement("p")
+        mainP.innerHTML = "Lorem ipsum dolor amet consectetur adipiscing dolore magna aliqua"
+        mainP.style.color = "#a6a7aa"
+        mainP.style.fontSize = "15px"
+        mainP.style.fontWeight = "400"
+        mainP.style.marginLeft = "80px"
+        mainP.style.marginBottom = "0"
+    main.appendChild(mainP)
+    var mainP = document.createElement("p")
+        mainP.innerHTML = "enim minim estudiat veniam siad venumus dolore"
+        mainP.style.color = "#a6a7aa"
+        mainP.style.fontSize = "15px"
+        mainP.style.fontWeight = "400"
+        mainP.style.marginLeft = "80px"
+        mainP.style.marginTop = "0px"
+    main.appendChild(mainP)
+    var mainButton = document.getElementById("main__button")
+    main.appendChild(mainButton)
+    function g(z){
+        z.style.transition = "all 0.5s ease"
+        z.style.backgroundColor = "white"
+        z.style.color = "grey"
+    }
+    function k(c){
+        c.style.color = "white"
+        c.style.backgroundColor = "inherit"
+    }
+document.body.appendChild(main)
+var sec1 = document.createElement("div")
+    var se1Div = document.createElement("div")
+        se1Div.style.width = "85%"
+        se1Div.style.margin = "0 auto"
+        se1Div.style.display = "flex"
+        se1Div.style.justifyContent = "space-between"
+        var sec1Mini = document.createElement("div")
+            sec1Mini.style.backgroundColor = "#e7e7e7"
+            sec1Mini.style.margin = "30px 0 30px 0"
+            sec1Mini.style.width = "24%"
+            sec1Mini.style.padding = "0 0 0 5px"
+            var sec1Img = document.createElement("img")
+                sec1Img.setAttribute("src","./image/outline_speed_black_18dp.png")
+            sec1Mini.appendChild(sec1Img)
+            sec1miniH = document.createElement("h2")
+                sec1miniH.innerHTML = "Art Of Coding"
+                sec1miniH.style.color = "#515769"
+            sec1Mini.appendChild(sec1miniH)
+            sec1miniP = document.createElement("p")
+                sec1miniP.innerHTML = "Lorem ipsum dolor amet consectetur ut consequat siad esqudiat dolor"
+                sec1miniP.style.color = "grey"
+            sec1Mini.appendChild(sec1miniP)
+        se1Div.appendChild(sec1Mini)
+        var sec1Mini = document.createElement("div")
+            sec1Mini.style.backgroundColor = "#e7e7e7"
+            sec1Mini.style.margin = "30px 0 30px 0"
+            sec1Mini.style.width = "24%"
+            sec1Mini.style.padding = "0 0 0 5px"
+            var sec1Img = document.createElement("img")
+                sec1Img.setAttribute("src","./image/outline_speed_black_18dp.png")
+            sec1Mini.appendChild(sec1Img)
+            sec1miniH = document.createElement("h2")
+                sec1miniH.innerHTML = "Art Of Coding"
+                sec1miniH.style.color = "#515769"
+            sec1Mini.appendChild(sec1miniH)
+            sec1miniP = document.createElement("p")
+                sec1miniP.innerHTML = "Lorem ipsum dolor amet consectetur ut consequat siad esqudiat dolor"
+                sec1miniP.style.color = "grey"
+            sec1Mini.appendChild(sec1miniP)
+        se1Div.appendChild(sec1Mini)
+        var sec1Mini = document.createElement("div")
+            sec1Mini.style.backgroundColor = "#e7e7e7"
+            sec1Mini.style.margin = "30px 0 30px 0"
+            sec1Mini.style.width = "24%"
+            sec1Mini.style.padding = "0 0 0 5px"
+            var sec1Img = document.createElement("img")
+                sec1Img.setAttribute("src","./image/outline_speed_black_18dp.png")
+            sec1Mini.appendChild(sec1Img)
+            sec1miniH = document.createElement("h2")
+                sec1miniH.innerHTML = "Art Of Coding"
+                sec1miniH.style.color = "#515769"
+            sec1Mini.appendChild(sec1miniH)
+            sec1miniP = document.createElement("p")
+                sec1miniP.innerHTML = "Lorem ipsum dolor amet consectetur ut consequat siad esqudiat dolor"
+                sec1miniP.style.color = "grey"
+            sec1Mini.appendChild(sec1miniP)
+        se1Div.appendChild(sec1Mini)
+    sec1.appendChild(se1Div)
+    var se1Div = document.createElement("div")
+        se1Div.style.width = "85%"
+        se1Div.style.margin = "0 auto"
+        se1Div.style.display = "flex"
+        se1Div.style.justifyContent = "space-between"
+        var sec1Mini = document.createElement("div")
+            sec1Mini.style.backgroundColor = "#e7e7e7"
+            sec1Mini.style.margin = "30px 0 30px 0"
+            sec1Mini.style.width = "24%"
+            sec1Mini.style.padding = "0 0 0 5px"
+            var sec1Img = document.createElement("img")
+                sec1Img.setAttribute("src","./image/outline_speed_black_18dp.png")
+            sec1Mini.appendChild(sec1Img)
+            sec1miniH = document.createElement("h2")
+                sec1miniH.innerHTML = "Art Of Coding"
+                sec1miniH.style.color = "#515769"
+            sec1Mini.appendChild(sec1miniH)
+            sec1miniP = document.createElement("p")
+                sec1miniP.innerHTML = "Lorem ipsum dolor amet consectetur ut consequat siad esqudiat dolor"
+                sec1miniP.style.color = "grey"
+            sec1Mini.appendChild(sec1miniP)
+        se1Div.appendChild(sec1Mini)
+        var sec1Mini = document.createElement("div")
+            sec1Mini.style.backgroundColor = "#e7e7e7"
+            sec1Mini.style.margin = "30px 0 30px 0"
+            sec1Mini.style.width = "24%"
+            sec1Mini.style.padding = "0 0 0 5px"
+            var sec1Img = document.createElement("img")
+                sec1Img.setAttribute("src","./image/outline_speed_black_18dp.png")
+            sec1Mini.appendChild(sec1Img)
+            sec1miniH = document.createElement("h2")
+                sec1miniH.innerHTML = "Art Of Coding"
+                sec1miniH.style.color = "#515769"
+            sec1Mini.appendChild(sec1miniH)
+            sec1miniP = document.createElement("p")
+                sec1miniP.innerHTML = "Lorem ipsum dolor amet consectetur ut consequat siad esqudiat dolor"
+                sec1miniP.style.color = "grey"
+            sec1Mini.appendChild(sec1miniP)
+        se1Div.appendChild(sec1Mini)
+        var sec1Mini = document.createElement("div")
+            sec1Mini.style.backgroundColor = "#e7e7e7"
+            sec1Mini.style.margin = "30px 0 30px 0"
+            sec1Mini.style.width = "24%"
+            sec1Mini.style.padding = "0 0 0 5px"
+            var sec1Img = document.createElement("img")
+                sec1Img.setAttribute("src","./image/outline_speed_black_18dp.png")
+            sec1Mini.appendChild(sec1Img)
+            sec1miniH = document.createElement("h2")
+                sec1miniH.innerHTML = "Art Of Coding"
+                sec1miniH.style.color = "#515769"
+            sec1Mini.appendChild(sec1miniH)
+            sec1miniP = document.createElement("p")
+                sec1miniP.innerHTML = "Lorem ipsum dolor amet consectetur ut consequat siad esqudiat dolor"
+                sec1miniP.style.color = "grey"
+            sec1Mini.appendChild(sec1miniP)
+        se1Div.appendChild(sec1Mini)
+    sec1.appendChild(se1Div)
+document.body.appendChild(sec1)
+var sec2 = document.createElement("div")
+    sec2.style.display = "flex"
+    sec2.style.justifyContent = "space-between"
+    sec2.style.padding = "100px"
+    var sec2Img = document.getElementById("sec2img1")
+    sec2.appendChild(sec2Img)
+    var sec2Img = document.getElementById("sec2img2")
+    sec2.appendChild(sec2Img)
+    var sec2Img = document.getElementById("sec2img3")
+    sec2.appendChild(sec2Img)
+    var sec2Img = document.getElementById("sec2img4")
+    sec2.appendChild(sec2Img)
+    var sec2Img = document.getElementById("sec2img5")
+    sec2.appendChild(sec2Img)
+    var sec2Img = document.getElementById("sec2img6")
+    sec2.appendChild(sec2Img)
+document.body.appendChild(sec2) 
